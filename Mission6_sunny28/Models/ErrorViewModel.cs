e80da42e9991a8927094d53d6cfc1e798a03e797
YuiZoldyck <betasan28@gmail.com>
2023-02-10 19:44:07 -0700

    First commit

---
+++ Mission6_sunny28/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Mission6_sunny28.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ Mission6_sunny28/Models/Movie.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Mission6_sunny28.Models
+{
+    public class Movie
+    {
+        public string Category { get; set; }
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public string Director { get; set; }
+        public string Rating{ get; set; }
+        public bool Edited { get; set; }
+        public string LentTo { get; set; }
+        public string Notes { get; set; }
+
+    }
+}
+
+
+//For the Rating field, use a dropdown menu (G, PG, PG-13, R).
+//o For the Edited field, we want that to be a yes/no (true/false) option.
+//o The “Edited”, “Lent To”, and “Notes” fields are optional. All other fields must be
+//entered.
+//o Notes should be limited to 25 characters.
